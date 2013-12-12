b0f15089b4285a4f272fdb1a1c7f1f08a652f677
Renaud Paquay <rpaquay@chromium.org>
2013-12-12 14:29:07 -0800

    Increase version to 0.9.0.

--- VsChromiumNative/resource.h
+++ VsChromiumNative/resource.h
@@ -1,10 +1,7 @@
-// Copyright 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by version.rc
+//
 
 // Next default values for new objects
 // 

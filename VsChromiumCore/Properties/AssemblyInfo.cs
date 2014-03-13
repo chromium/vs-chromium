4524bb9111385caab3b7d76c21b1d76d8b71196c
Zachary Turner <zturner@chromium.org>
2014-03-13 16:51:29 -0700

    Port VsChromium to VS2013.

--- VsChromiumCore/Properties/AssemblyInfo.cs
+++ VsChromiumCore/Properties/AssemblyInfo.cs
@@ -8,24 +8,21 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("VsChromiumCore")]
 [assembly: AssemblyDescription("Library conaining code common to VsChromiumPackage and VsChromiumServer.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Chromium Authors")]
 [assembly: AssemblyProduct("VsChromiumCore")]
-[assembly: AssemblyCopyright("Copyright 2013 The Chromium Authors. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2014 The Chromium Authors. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("ee523bd4-3d9f-4ce6-bac0-530da6fd6239")]
 
 // Version information for an assembly consists of the following four values:
@@ -38,6 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("0.9.4.0")]
 [assembly: AssemblyFileVersion("0.9.4.0")]

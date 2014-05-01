226fd6a41b5cdcfb2c3a8254f7648d506f3b86e3
Renaud Paquay <rpaquay@chromium.org>
2014-05-01 13:28:25 -0700

    Fix potential bug with FileSystemName comparison.

--- src/Server/FileSystemNames/FileSystemNameComparer.cs
+++ src/Server/FileSystemNames/FileSystemNameComparer.cs
@@ -12,18 +12,47 @@ namespace VsChromium.Server.FileSystemNames {
     public static FileSystemNameComparer Instance { get { return _instance; } }
 
     public int Compare(FileSystemName x, FileSystemName y) {
+      if (x == null) {
+        if (y == null)
+          return 0;
+        else
+          return -1;
+      } else {
+        if (y == null)
+          return 1;
+      }
+
       var x1 = GetAbsolutePart(x);
       var y1 = GetAbsolutePart(y);
       var result = x1.FullPathName.CompareTo(y1.FullPathName);
       if (result == 0)
         result = x.RelativePathName.CompareTo(y.RelativePathName);
+      else {
+        // If the absolute parts are not equal, we may be in a case where we
+        // have 2 distinct representations of the same canonical path name.
+        // Instead of implementing a complex algorithm, just compare the full
+        // paths (at the cost of a string concatenation).
+        return x.FullPathName.CompareTo(y.FullPathName);
+      }
       return result;
     }
 
     public bool Equals(FileSystemName x, FileSystemName y) {
       if (x == null || y == null)
         return object.ReferenceEquals(x, y);
-      return Compare(x, y) == 0;
+      var x1 = GetAbsolutePart(x);
+      var y1 = GetAbsolutePart(y);
+      var result = x1.FullPathName.Equals(y1.FullPathName);
+      if (result)
+        result = x.RelativePathName.Equals(y.RelativePathName);
+      else {
+        // If the absolute parts are not equal, we may be in a case where we
+        // have 2 distinct representations of the same canonical path name.
+        // Instead of implementing a complex algorithm, just compare the full
+        // paths (at the cost of a string concatenation).
+        return x.FullPathName.Equals(y.FullPathName);
+      }
+      return result;
     }
 
     public int GetHashCode(FileSystemName x) {

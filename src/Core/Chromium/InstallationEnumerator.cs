292ffa66c0bfae27769f465814018819cf98ebef
Renaud Paquay <rpaquay@chromium.org>
2014-06-11 13:51:24 -0700

    Fix crash with unexpected registry key values.

--- src/Core/Chromium/InstallationEnumerator.cs
+++ src/Core/Chromium/InstallationEnumerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Microsoft.Win32;
@@ -20,19 +21,15 @@ namespace VsChromium.Core.Chromium {
     private IEnumerable<InstallationData> Enumerate(InstallationLevel level) {
       List<InstallationData> results = new List<InstallationData>();
 
-      RegistryHive hive = (level == InstallationLevel.User) 
-          ? RegistryHive.CurrentUser 
+      RegistryHive hive = (level == InstallationLevel.User)
+          ? RegistryHive.CurrentUser
           : RegistryHive.LocalMachine;
-      using (RegistryKey baseKey = RegistryKey.OpenBaseKey(hive, RegistryView.Default)) {
+      using (RegistryKey baseKey = RegistryKey.OpenBaseKey(hive, RegistryView.Default))
       using (RegistryKey uninstallKey = baseKey.OpenSubKey(kUninstallSubKey)) {
         foreach (string subkeyName in uninstallKey.GetSubKeyNames()) {
           using (RegistryKey subkey = uninstallKey.OpenSubKey(subkeyName)) {
-            string location = (string)subkey.GetValue("InstallLocation");
-            if (location == null)
-              continue;
-
-            string exePath = Path.Combine(location, "chrome.exe");
-            if (!File.Exists(exePath))
+            string exePath = GetChromeExePath(subkey);
+            if (string.IsNullOrEmpty(exePath))
               continue;
 
             int iconIndex = 0;
@@ -50,8 +47,28 @@ namespace VsChromium.Core.Chromium {
           }
         }
       }
-      }
       return results;
+    }
+
+    private static string GetChromeExePath(RegistryKey subkey) {
+      var location = (string)subkey.GetValue("InstallLocation");
+      if (location == null)
+        return null;
+
+      // Sometimes the location is surrounded by quotes. Remove them.
+      location = location.Trim('"');
+
+      // Swallowing exceptions here, as we may get invalid characters, invalid paths, etc.
+      try {
+        var exePath = Path.Combine(location, "chrome.exe");
+        if (!File.Exists(exePath))
+          return null;
+        return exePath;
+      }
+      catch (Exception e) {
+        Logger.LogException(e, "InstallLocation seems to be invalid: {0}", location);
+        return null;
+      }
     }
   }
 }

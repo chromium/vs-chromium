f63d6f031c0d2f8b7106dc37a962488c3fc57d7c
Renaud Paquay <rpaquay@chromium.org>
2014-11-13 14:58:09 -0800

    Fix path comparison to properly support path seperators.
    "foo-1\bar" should appear *after* "foo\bar" in sorted lists.

--- src/Core/Files/CaseInsensitivePathComparer.cs
+++ src/Core/Files/CaseInsensitivePathComparer.cs
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 using System;
+using System.IO;
 
 namespace VsChromium.Core.Files {
   public class CaseInsensitivePathComparer : IPathComparer {
@@ -10,8 +11,68 @@ namespace VsChromium.Core.Files {
 
     public static IPathComparer Instance { get { return _theInstance; } }
 
-    public StringComparer Comparer { get { return StringComparer.OrdinalIgnoreCase; } }
+    public StringComparer Comparer { get { return CustomPathComparer.Instance; } }
 
     public StringComparison Comparison { get { return StringComparison.OrdinalIgnoreCase; } }
+  }
+
+  public class CustomPathComparer : StringComparer {
+    private static readonly CustomPathComparer _theInstance = new CustomPathComparer();
+    private static readonly char[] _directorySeparators = { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
+
+    public static CustomPathComparer Instance { get { return _theInstance; } }
+
+    public override int Compare(string x, string y) {
+      if (object.ReferenceEquals(x, y))
+        return 0;
+
+      // Compare individual components, using "\" as seperator.
+      int startIndex = 0;
+      int length = Math.Min(x.Length, y.Length);
+      while (startIndex < length) {
+        var idx = x.IndexOfAny(_directorySeparators, startIndex);
+        var idy = y.IndexOfAny(_directorySeparators, startIndex);
+        if (idx < 0) {
+          if (idy < 0) {
+            var result = string.Compare(x, startIndex, y, startIndex, length - startIndex,
+              StringComparison.OrdinalIgnoreCase);
+            if (result != 0) {
+              return result;
+            } else {
+              break;
+            }
+          } else {
+            // y has one additional component => x < y
+            return -1;
+          }
+        } else {
+          if (idy < 0) {
+            // x has one additional component => x > y
+            return 1;
+          } else {
+            if (idx != idy) {
+              // Current path component of x or y is longer => return longest.
+              return idx - idy;
+            }
+            var result = string.Compare(x, startIndex, y, startIndex, idx - startIndex - 1, StringComparison.OrdinalIgnoreCase);
+            if (result != 0)
+              return result;
+            startIndex = idx + 1;
+          }
+        }
+      }
+
+      // All common characters are equal to this point, use length
+      // as final decision point.
+      return x.Length - y.Length;
+    }
+
+    public override bool Equals(string x, string y) {
+      return StringComparer.OrdinalIgnoreCase.Equals(x, y);
+    }
+
+    public override int GetHashCode(string obj) {
+      return StringComparer.OrdinalIgnoreCase.GetHashCode(obj);
+    }
   }
 }
---
+++ src/Tests/TestPathUtils.cs
@@ -0,0 +1,26 @@
+﻿// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VsChromium.Core.Files;
+
+namespace VsChromium.Tests {
+  [TestClass]
+  public class TestPathUtils {
+    [TestMethod]
+    public void CustomPathComparerWorks() {
+      var comparer = CustomPathComparer.Instance;
+      Assert.IsTrue(comparer.Compare(@"foo", @"foo") == 0);
+      Assert.IsTrue(comparer.Compare(@"foo\bar", @"foo\bar") == 0);
+      Assert.IsTrue(comparer.Compare(@"foo/bar", @"foo/bar") == 0);
+      Assert.IsTrue(comparer.Compare(@"foo/bar", @"foo/bar") == 0);
+      Assert.IsTrue(comparer.Compare(@"foo/bar", @"foo\bar") == 0);
+      Assert.IsTrue(comparer.Compare(@"foo\bar2", @"foo\bar") > 0);
+      Assert.IsTrue(comparer.Compare(@"foo\bar", @"foo\bar2") < 0);
+      Assert.IsTrue(comparer.Compare(@"foo1\bar", @"foo\bar") > 0);
+      Assert.IsTrue(comparer.Compare(@"foo-1\bar", @"foo\bar") > 0);
+      Assert.IsTrue(comparer.Compare(@"foo\bar", @"foo-1\bar") < 0);
+    }
+  }
+}

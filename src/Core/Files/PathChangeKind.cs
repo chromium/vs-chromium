89a7583994db2549a4c9656cd2f4a81965960029
Renaud Paquay <rpaquay@chromium.org>
2014-11-21 14:16:35 -0800

    Copyright headers.

--- src/Core/Files/PathChangeKind.cs
+++ src/Core/Files/PathChangeKind.cs
@@ -1,4 +1,8 @@
-﻿namespace VsChromium.Core.Files {
+﻿// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace VsChromium.Core.Files {
   public enum PathChangeKind {
     None,
     Created,

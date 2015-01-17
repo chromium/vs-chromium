17ca632d3aac023ec8dab0463b9337d73fb80369
Renaud Paquay <rpaquay@chromium.org>
2015-01-16 18:16:15 -0800

    Fix strstr search (using std::search instead)

--- src/Native/search_strstr.cpp
+++ src/Native/search_strstr.cpp
@@ -6,24 +6,35 @@
 
 #include "search_strstr.h"
 
+#include <algorithm>
+
 StrStrSearch::StrStrSearch()
     : pattern_(NULL),
       patternLen_(0) {
 }
 
-void StrStrSearch::PreProcess(const char *pattern, int patternLen, SearchOptions options, SearchCreateResult& result) {
+void StrStrSearch::PreProcess(
+    const char *pattern,
+    int patternLen,
+    SearchOptions options,
+    SearchCreateResult& result) {
   pattern_ = pattern;
   patternLen_ = patternLen;
 }
 
 void StrStrSearch::Search(SearchParams* searchParams) {
   const char* start = searchParams->TextStart;
+  const char* last = searchParams->TextStart + searchParams->TextLength;
   if (searchParams->MatchStart) {
     start = searchParams->MatchStart + searchParams->MatchLength;
   }
 
-  searchParams->MatchStart = strstr(start, pattern_);
-  if (searchParams->MatchStart != nullptr) {
-    searchParams->MatchLength = patternLen_;
+  auto result = std::search(start, last, pattern_, pattern_ + patternLen_);
+  if (result == last) {
+    searchParams->MatchStart = nullptr;
+    searchParams->MatchLength = 0;
+    return;
   }
+  searchParams->MatchStart = result;
+  searchParams->MatchLength = patternLen_;
 }

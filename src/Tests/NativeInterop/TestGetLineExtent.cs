e59be651ed28f8e0a117516541e5157ccffc7ed4
Renaud Paquay <rpaquay@chromium.org>
2015-02-01 22:14:29 -0800

    Clean up test code.

--- src/Tests/NativeInterop/TestGetLineExtent.cs
+++ src/Tests/NativeInterop/TestGetLineExtent.cs
@@ -15,17 +15,7 @@ namespace VsChromium.Tests.NativeInterop {
     public void GetLineExtentForShortLineWorks() {
       using (var mem = CreateAsciiMemory("this is a test")) {
         for (var i = 0; i < mem.Size - 1; i++) {
-          int offset;
-          int length;
-          NativeMethods.Ascii_GetLineExtentFromPosition(
-            mem.Ptr,
-            mem.Size - 1,
-            i,
-            100,
-            out offset,
-            out length);
-          Assert.AreEqual(0, offset);
-          Assert.AreEqual(14, length);
+          AssertExtent(mem, i, 100, 0, 14);
         }
       }
     }
@@ -34,43 +24,13 @@ namespace VsChromium.Tests.NativeInterop {
     public void GetLineExtentForMultilineWorks() {
       using (var mem = CreateAsciiMemory("this is a test\nand another line\nline3")) {
         for (var i = 0; i <= 14; i++) {
-          int offset;
-          int length;
-          NativeMethods.Ascii_GetLineExtentFromPosition(
-            mem.Ptr,
-            mem.Size - 1,
-            i,
-            100,
-            out offset,
-            out length);
-          Assert.AreEqual(0, offset);
-          Assert.AreEqual(15, length);
+          AssertExtent(mem, i, 100, 0, 15);
         }
         for (var i = 15; i <= 31; i++) {
-          int offset;
-          int length;
-          NativeMethods.Ascii_GetLineExtentFromPosition(
-            mem.Ptr,
-            mem.Size - 1,
-            i,
-            100,
-            out offset,
-            out length);
-          Assert.AreEqual(15, offset);
-          Assert.AreEqual(17, length);
+          AssertExtent(mem, i, 100, 15, 17);
         }
         for (var i = 32; i <= 36; i++) {
-          int offset;
-          int length;
-          NativeMethods.Ascii_GetLineExtentFromPosition(
-            mem.Ptr,
-            mem.Size - 1,
-            i,
-            100,
-            out offset,
-            out length);
-          Assert.AreEqual(32, offset);
-          Assert.AreEqual(5, length);
+          AssertExtent(mem, i, 100, 32, 5);
         }
       }
     }
@@ -79,19 +39,34 @@ namespace VsChromium.Tests.NativeInterop {
     public void GetLineExtentForEmptyWorks() {
       using (var mem = CreateAsciiMemory("\n\n\n")) {
         for (var i = 0; i <= 2; i++) {
-          int offset;
-          int length;
-          NativeMethods.Ascii_GetLineExtentFromPosition(
-            mem.Ptr,
-            mem.Size - 1,
-            i,
-            100,
-            out offset,
-            out length);
-          Assert.AreEqual(i, offset);
-          Assert.AreEqual(1, length);
+          AssertExtent(mem, i, 100, i, 1);
         }
       }
+    }
+
+    [TestMethod]
+    public void GetLineExtentWithMaxLengthWorks() {
+      using (var mem = CreateAsciiMemory("this is a test\nand another line\nline3")) {
+        AssertExtent(mem, 0, 3, 0, 3);
+        AssertExtent(mem, 1, 3, 0, 4);
+        AssertExtent(mem, 5, 3, 2, 6);
+        AssertExtent(mem, 13, 3, 10, 5);
+        AssertExtent(mem, 14, 3, 11, 4);
+      }
+    }
+
+    public void AssertExtent(MemoryBlock mem, int position, int maxLength, int expectedOffset, int expectedLength) {
+      int offset;
+      int length;
+      NativeMethods.Ascii_GetLineExtentFromPosition(
+        mem.Ptr,
+        mem.Size - 1,
+        position,
+        maxLength,
+        out offset,
+        out length);
+      Assert.AreEqual(expectedOffset, offset);
+      Assert.AreEqual(expectedLength, length);
     }
 
     public static MemoryBlock CreateAsciiMemory(string text) {

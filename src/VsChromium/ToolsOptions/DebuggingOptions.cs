2d841d2625e79927827e85435083ce2b6b1b50fc
Renaud Paquay <rpaquay@chromium.org>
2015-02-21 12:17:50 -0800

    Implement "ResetSettings", raise event when settings change.

--- src/VsChromium/ToolsOptions/DebuggingOptions.cs
+++ src/VsChromium/ToolsOptions/DebuggingOptions.cs
@@ -4,13 +4,30 @@
 
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Shell;
+using VsChromium.Core.Logging;
+using VsChromium.Package;
 
 namespace VsChromium.ToolsOptions {
   [ClassInterface(ClassInterfaceType.AutoDual)]
   //[CLSCompliant(false)]
   [ComVisible(true)]
   public class DebuggingOptions : DialogPage {
+    public DebuggingOptions() {
+      SetDefaults();
+    }
+
+    private void SetDefaults() {
+      // Default.
+      EnableChildDebugging = false;
+    }
+
+    public override void ResetSettings() {
+      base.ResetSettings();
+      SetDefaults();
+    }
+
     [Category("General")]
     [DisplayName("Enable Child Process Debugging")]
     [Description("Enable child-process debugging for launch/attach scenarios other than those available through the Attach to Chrome dialog.")]
--- src/VsChromium/ToolsOptions/GeneralOptions.cs
+++ src/VsChromium/ToolsOptions/GeneralOptions.cs
@@ -2,9 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Shell;
+using VsChromium.Core.Logging;
+using VsChromium.Package;
 
 namespace VsChromium.ToolsOptions {
   [ClassInterface(ClassInterfaceType.AutoDual)]
@@ -12,12 +16,37 @@ namespace VsChromium.ToolsOptions {
   [ComVisible(true)]
   public class GeneralOptions : DialogPage {
     public GeneralOptions() {
+      SetDefaults();
+    }
+
+    private void SetDefaults() {
       // Default.
       EnableVsChromiumProjects = true;
       FindInFilesMaxEntries = 10 * 1000;
       SearchFileNamesMaxEntries = 2 * 1000;
       MaxTextExtractLength = 120;
       AutoSearchDelayMsec = 20;
+    }
+
+    public override void ResetSettings() {
+      base.ResetSettings();
+      SetDefaults();
+    }
+
+    protected override void OnApply(PageApplyEventArgs e) {
+      base.OnApply(e);
+
+      if (e.ApplyBehavior != ApplyKind.Apply)
+        return;
+
+      var componentModel = (IComponentModel)this.GetService(typeof(SComponentModel));
+      if (componentModel == null) {
+        Logger.LogError("No component model found in DialogPage.");
+        return;
+      }
+
+      var bus = componentModel.DefaultExportProvider.GetExportedValue<IEventBus>();
+      bus.Fire("GlobalSettingsChanged", this, new EventArgs());
     }
 
     [Category("Projects")]

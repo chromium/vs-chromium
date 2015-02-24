99a9c8df8675015dac8b889053897b0ab7ecc291
Renaud Paquay <rpaquay@chromium.org>
2015-02-24 12:59:10 -0800

    Headers.

--- src/VsChromium/Package/CommandHandler/PackagePriorityCommandHandlerBase.cs
+++ src/VsChromium/Package/CommandHandler/PackagePriorityCommandHandlerBase.cs
@@ -1,4 +1,8 @@
-﻿namespace VsChromium.Package.CommandHandler {
+﻿// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace VsChromium.Package.CommandHandler {
   public abstract class PackagePriorityCommandHandlerBase : PackageCommandHandlerBase, IPackagePriorityCommandHandler {
   }
 }
--- src/VsChromium/Package/CommandHandler/PackagePriorityCommandHandlerRegistration.cs
+++ src/VsChromium/Package/CommandHandler/PackagePriorityCommandHandlerRegistration.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;

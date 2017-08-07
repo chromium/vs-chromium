7ff6319c6cf933b000a3a9e8f8d745729c110cc5
Renaud Paquay <rpaquay@google.com>
2017-08-07 08:20:37 -0700

    Bump version #

--- src/Core/VsChromiumVersion.cs
+++ src/Core/VsChromiumVersion.cs
@@ -4,7 +4,7 @@
 
 namespace VsChromium.Core {
   public static class VsChromiumVersion {
-    public const string Product = "0.9.23";
+    public const string Product = "0.9.24";
     public const string File = Product + ".0";
   }
 }

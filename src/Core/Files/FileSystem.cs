669f205046048eb0d8c6f0cbacb7fdb6ea1595ca
Renaud Paquay <rpaquay@google.com>
2017-11-07 12:23:41 -0800

    Don't enumerate directories entries longer than MAX_PATH
    This is to avoid running into long paths issue later on.

--- src/Core/Files/FileSystem.cs
+++ src/Core/Files/FileSystem.cs
@@ -6,12 +6,18 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using VsChromium.Core.Logging;
 using VsChromium.Core.Win32.Files;
 using VsChromium.Core.Win32.Memory;
 
 namespace VsChromium.Core.Files {
   [Export(typeof(IFileSystem))]
   public class FileSystem : IFileSystem {
+    private const long MAX_PATH_TOO_LOG_LOG_COUNT = 10;
+    private long _pathTooLongErrorCount;
+
     public IFileInfoSnapshot GetFileInfoSnapshot(FullPath path) {
       return new FileInfoSnapshot(path);
     }
@@ -37,7 +43,31 @@ namespace VsChromium.Core.Files {
     }
 
     public IList<DirectoryEntry> GetDirectoryEntries(FullPath path) {
-      return NativeFile.GetDirectoryEntries(path.Value);
+      var list = NativeFile.GetDirectoryEntries(path.Value);
+      // Skip any entry that is longer than MAX_PATH.
+      // Fix this once we fully support the long path syntax ("\\?\" prefix)
+      if (list.Any(entry => PathHelpers.IsPathTooLong(path.Value, entry.Name))) {
+        return list.Where(entry => {
+          if (PathHelpers.IsPathTooLong(path.Value, entry.Name)) {
+            // Note: The condition is unsafe from a pure concurrency point of view,
+            //       but is ok in this case because the field is incrementally increasing.
+            //       This is just an optimization to avoid an Interlocked call.
+            if (_pathTooLongErrorCount <= MAX_PATH_TOO_LOG_LOG_COUNT) {
+              var logCount = Interlocked.Increment(ref _pathTooLongErrorCount);
+              if (logCount <= MAX_PATH_TOO_LOG_LOG_COUNT) {
+                Logger.LogInfo("Skipping directory entry because path is too long: \"{0}\"",
+                  path.Combine(new RelativePath(entry.Name)));
+              }
+              if (logCount == MAX_PATH_TOO_LOG_LOG_COUNT) {
+                Logger.LogInfo("  (Note: No more message abount path too long will be logged, because the maximum number of occurrences has been reached)");
+              }
+            }
+            return false;
+          }
+          return true;
+        }).ToList();
+      }
+      return list;
     }
   }
 }

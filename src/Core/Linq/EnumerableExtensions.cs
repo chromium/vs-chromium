68f55244e7ab4b487a05dd794e19c439f2c9b413
Renaud Paquay <rpaquay@google.com>
2018-02-03 12:14:51 -0800

    Remove unused type (IReadOnlyMap)

--- src/Core/Linq/EnumerableExtensions.cs
+++ src/Core/Linq/EnumerableExtensions.cs
@@ -283,23 +283,6 @@ namespace VsChromium.Core.Linq {
       return value;
     }
 
-    /// <summary>
-    /// Returns <code>null</code> when not found.
-    /// </summary>
-    public static TValue GetValue<TKey, TValue>(this IReadOnlyMap<TKey, TValue> dictionary, TKey key) where TValue : class {
-      return dictionary.GetValue(key, null);
-    }
-
-    /// <summary>
-    /// Returns <code>defaultValue</code> when not found.
-    /// </summary>
-    public static TValue GetValue<TKey, TValue>(this IReadOnlyMap<TKey, TValue> dictionary, TKey key, TValue defaultValue) {
-      TValue result;
-      if (!dictionary.TryGetValue(key, out result))
-        return defaultValue;
-      return result;
-    }
-
     public static void RemoveWhere<TKey, TValue>(
       this IDictionary<TKey, TValue> source,
       Func<KeyValuePair<TKey, TValue>, bool> predicate) {

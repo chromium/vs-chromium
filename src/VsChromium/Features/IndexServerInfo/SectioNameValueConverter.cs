02376a37c856e19093bca91d37f0dfb977f95022
Renaud Paquay <rpaquay@google.com>
2018-02-09 17:15:47 -0800

    UI consistency

---
+++ src/VsChromium/Features/IndexServerInfo/SectioNameValueConverter.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace VsChromium.Features.IndexServerInfo {
+  public class SectioNameValueConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      var s = value as string;
+      if (string.IsNullOrEmpty(s)) {
+        return "n/a";
+      }
+
+      return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+      throw new NotImplementedException();
+    }
+  }
+}

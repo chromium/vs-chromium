914b8de7bbfddd5b9c3ab97304111b2c86b16dc8
Renaud Paquay <rpaquay@google.com>
2018-02-13 09:20:49 -0800

    Show initial sort anchor in list view
    Also change default sort order to "Descending" for size columns

--- src/VsChromium/Features/IndexServerInfo/DirectoryDetailsControl.xaml.cs
+++ src/VsChromium/Features/IndexServerInfo/DirectoryDetailsControl.xaml.cs
@@ -10,15 +10,15 @@ using System.Windows.Media;
 
 namespace VsChromium.Features.IndexServerInfo {
   public partial class DirectoryDetailsControl {
-    private readonly Sorter _filesByExtensionListViewSorter;
-    private readonly Sorter _largeFilesListViewSorter;
-    private readonly Sorter _largeBinaryFilesListViewSorter;
+    private readonly ListViewSorter _filesByExtensionListViewSorter;
+    private readonly ListViewSorter _largeFilesListViewSorter;
+    private readonly ListViewSorter _largeBinaryFilesListViewSorter;
 
     public DirectoryDetailsControl() {
       InitializeComponent();
-      _filesByExtensionListViewSorter = new Sorter(FilesByExtensionListView);
-      _largeFilesListViewSorter = new Sorter(LargeFilesListView);
-      _largeBinaryFilesListViewSorter = new Sorter(LargeBinaryFilesListView);
+      _filesByExtensionListViewSorter = new ListViewSorter(FilesByExtensionListView, nameof(FilesByExtensionListView));
+      _largeFilesListViewSorter = new ListViewSorter(LargeFilesListView, nameof(LargeFilesListView));
+      _largeBinaryFilesListViewSorter = new ListViewSorter(LargeBinaryFilesListView, nameof(LargeBinaryFilesListView));
     }
 
     private void ListViewColumnHeader_Click(object sender, RoutedEventArgs e) {
@@ -33,30 +33,71 @@ namespace VsChromium.Features.IndexServerInfo {
       _largeBinaryFilesListViewSorter.HeaderClick(sender);
     }
 
-    public class Sorter {
+    public class ListViewSorter {
       private readonly ListView _listView;
-      private GridViewColumnHeader _listViewSortCol;
-      private SortAdorner _listViewSortAdorner;
+      private readonly string _name;
+      private bool _firstLoad = true;
+      private GridViewColumnHeader _currentSortedColumnHeader;
+      private SortAdorner _currentAdorner;
 
-      public Sorter(ListView listView) {
+      public ListViewSorter(ListView listView, string name) {
         _listView = listView;
+        _name = name;
+        _listView.Loaded += (sender, args) => OnListViewLoaded();
+      }
+
+      private void OnListViewLoaded() {
+        if (!_firstLoad) {
+          return;
+        }
+
+        var gridView = _listView.View as GridView;
+        if (gridView == null) {
+          return;
+        }
+
+        foreach (var column in gridView.Columns) {
+          var header = column.Header as GridViewColumnHeader;
+          if (header != null) {
+            // The "Loaded" event is called multiple times for UI elements inside
+            // a tab control. The first "Loaded" event is called when the control
+            // is not visible yet, meaning there are no adornment layers for the column
+            // headers of the listview. The next "Loaded" events are called when
+            // the containing TabItem is activated. We use the trick below
+            // to figure out when it is safe to add the "Sorter" adornment, but
+            // we must do it only once.
+            if (AdornerLayer.GetAdornerLayer(header) != null) {
+              _firstLoad = false;
+              if (Wpf.ListViewSorter.GetInitialSortColumn(header)) {
+                HeaderClick(header);
+              }
+            }
+          }
+        }
       }
 
       public void HeaderClick(object sender) {
-        var column = (sender as GridViewColumnHeader);
+        HeaderClick(sender as GridViewColumnHeader);
+      }
+
+      public void HeaderClick(GridViewColumnHeader column) {
+        if (column == null) {
+          return;
+        }
         var sortBy = column.Tag.ToString();
-        if (_listViewSortCol != null) {
-          AdornerLayer.GetAdornerLayer(_listViewSortCol).Remove(_listViewSortAdorner);
+        if (_currentSortedColumnHeader != null) {
+          AdornerLayer.GetAdornerLayer(_currentSortedColumnHeader).Remove(_currentAdorner);
           _listView.Items.SortDescriptions.Clear();
         }
 
-        ListSortDirection newDir = ListSortDirection.Ascending;
-        if (_listViewSortCol == column && _listViewSortAdorner.Direction == newDir)
-          newDir = ListSortDirection.Descending;
+        var newDir = Wpf.ListViewSorter.GetInitialSortOrder(column);
+        if (_currentSortedColumnHeader == column && _currentAdorner.Direction == newDir) {
+          newDir = newDir == ListSortDirection.Descending ? ListSortDirection.Ascending : ListSortDirection.Descending;
+        }
 
-        _listViewSortCol = column;
-        _listViewSortAdorner = new SortAdorner(_listViewSortCol, newDir);
-        AdornerLayer.GetAdornerLayer(_listViewSortCol).Add(_listViewSortAdorner);
+        _currentSortedColumnHeader = column;
+        _currentAdorner = new SortAdorner(_currentSortedColumnHeader, newDir);
+        AdornerLayer.GetAdornerLayer(_currentSortedColumnHeader).Add(_currentAdorner);
         _listView.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
       }
     }
---
+++ src/VsChromium/Wpf/ListViewSorter.cs
@@ -0,0 +1,37 @@
+﻿// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace VsChromium.Wpf {
+  public class ListViewSorter {
+    public static readonly DependencyProperty InitialSortOrderProperty =
+      DependencyProperty.RegisterAttached("InitialSortOrder", typeof(ListSortDirection), typeof(ListViewSorter),
+        new FrameworkPropertyMetadata(ListSortDirection.Ascending));
+
+    public static readonly DependencyProperty InitialSortColumnProperty =
+      DependencyProperty.RegisterAttached("InitialSortColumn", typeof(bool), typeof(ListViewSorter),
+        new FrameworkPropertyMetadata(false));
+
+    public static void SetInitialSortOrder(GridViewColumnHeader obj, ListSortDirection value) {
+      obj.SetValue(InitialSortOrderProperty, value);
+
+    }
+
+    public static ListSortDirection GetInitialSortOrder(GridViewColumnHeader obj) {
+      return (ListSortDirection) obj.GetValue(InitialSortOrderProperty);
+    }
+
+    public static void SetInitialSortColumn(GridViewColumnHeader obj, bool value) {
+      obj.SetValue(InitialSortColumnProperty, value);
+
+    }
+
+    public static bool GetInitialSortColumn(GridViewColumnHeader obj) {
+      return (bool)obj.GetValue(InitialSortColumnProperty);
+    }
+  }
+}

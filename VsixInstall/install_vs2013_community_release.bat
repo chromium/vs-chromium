809ecffa702c3afb184b8dbc1cc3137ddbf96b00
Renaud Paquay <rpaquay@google.com>
2019-06-04 11:07:44 -0700

    Add vs 2013 helper files for vsix install/uninstall

---
+++ VsixInstall/install_vs2013_community_release.bat
@@ -0,0 +1,5 @@
+REM Copyright 2013 The Chromium Authors. All rights reserved.
+REM Use of this source code is governed by a BSD-style license that can be
+REM found in the LICENSE file.
+
+call install_vsix_helper.bat Release Community 12.0

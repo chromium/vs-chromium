1b1c291027d6025454a2599a8687d86baeccab60
Renaud Paquay <rpaquay@google.com>
2020-02-12 09:23:45 -0800

    Bump version to 0.9.33

--- src/Core/VsChromiumVersion.cs
+++ src/Core/VsChromiumVersion.cs
@@ -4,7 +4,7 @@
 
 namespace VsChromium.Core {
   public static class VsChromiumVersion {
-    public const string Product = "0.9.32";
+    public const string Product = "0.9.33";
     public const string File = Product + ".0";
     public const string Copyright = "Copyright © 2018 The Chromium Authors.All rights reserved.";
   }

3eaa64ebfff5133d5f7f6e658484c2eb53dce486
Renaud Paquay <rpaquay@google.com>
2020-06-02 09:58:48 -0700

    Update VSIX helpers to latest VS 2019 version

--- VsixInstall/install_vs2019_community_release.bat
+++ VsixInstall/install_vs2019_community_release.bat
@@ -2,4 +2,4 @@ REM Copyright 2015 The Chromium Authors. All rights reserved.
 REM Use of this source code is governed by a BSD-style license that can be
 REM found in the LICENSE file.
 
-call install_vsix_helper.bat Release Community 16.0
+call install_vsix_helper.bat Release Community 16.6

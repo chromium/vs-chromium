50a8f95ba22f3bba2fa73f573a8aeb939ac418b3
Renaud Paquay <rpaquay@google.com>
2020-06-11 14:50:44 -0700

    Fix VS 2017 VSIX installer helpers

--- VsixInstall/install_vs2017_community_release.bat
+++ VsixInstall/install_vs2017_community_release.bat
@@ -2,4 +2,4 @@ REM Copyright 2015 The Chromium Authors. All rights reserved.
 REM Use of this source code is governed by a BSD-style license that can be
 REM found in the LICENSE file.
 
-call install_vsix_helper.bat Release Community 15.0
+call install_vsix_helper.bat Release Community 15.9

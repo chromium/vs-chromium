903d2beb63e990eaabed7c0e9135ba69412bbdee
Renaud Paquay <rpaquay@google.com>
2020-06-22 11:38:09 -0700

    Run update notification banner on UI thread
    This fixes issue #69.

--- src/VsChromium/Features/AutoUpdate/IUpdateNotificationListener.cs
+++ src/VsChromium/Features/AutoUpdate/IUpdateNotificationListener.cs
@@ -4,6 +4,10 @@ namespace VsChromium.Features.AutoUpdate {
   /// available" event.
   /// </summary>
   public interface IUpdateNotificationListener {
+    /// <summary>
+    /// Method invoked when there is a new update available. The method is called
+    /// on a background thread.
+    /// </summary>
     void NotifyUpdate(UpdateInfo updateInfo);
   }
 }
--- src/VsChromium/Features/AutoUpdate/UpdateNotificationListener.cs
+++ src/VsChromium/Features/AutoUpdate/UpdateNotificationListener.cs
@@ -1,18 +1,24 @@
 using System.ComponentModel.Composition;
 using VsChromium.Features.ToolWindows;
+using VsChromium.Threads;
 
 namespace VsChromium.Features.AutoUpdate {
   [Export(typeof(IUpdateNotificationListener))]
   public class UpdateNotificationListener : IUpdateNotificationListener {
     private readonly IToolWindowAccessor _toolWindowAccessor;
+    private readonly ISynchronizationContextProvider _synchronizationContextProvider;
 
     [ImportingConstructor]
-    public UpdateNotificationListener(IToolWindowAccessor toolWindowAccessor) {
+    public UpdateNotificationListener(IToolWindowAccessor toolWindowAccessor, ISynchronizationContextProvider synchronizationContextProvider) {
       _toolWindowAccessor = toolWindowAccessor;
+      _synchronizationContextProvider = synchronizationContextProvider;
     }
 
     public void NotifyUpdate(UpdateInfo updateInfo) {
-      _toolWindowAccessor.CodeSearch.NotifyPackageUpdate(updateInfo);
+      // We have to run this on the UI thread since we are accessing UI components
+      _synchronizationContextProvider.DispatchThreadContext.Post(() => {
+        _toolWindowAccessor.CodeSearch.NotifyPackageUpdate(updateInfo);
+      });
     }
   }
 }
